9bdc2dccd7f310d82b23899efecc5571c20566f0
Luka <lukadjent@gmail.com>
2020-12-23 12:05:23 +0200

    1

--- src/main/java/museum/Educatable.java
+++
@@ -1,5 +0,0 @@
-package museum;
-
-public interface Educatable {
-    <T extends EntertainmentPlace> T educate();
-}
--- src/main/java/museum/exceptions/NotEnoughExhibitsException.java
+++
@@ -1,7 +0,0 @@
-package museum.exceptions;
-
-public class NotEnoughExhibitsException extends RuntimeException {
-    public NotEnoughExhibitsException(String message) {
-        super(message);
-    }
-}
